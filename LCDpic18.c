cedbbdaa3904ce5b0beccd4dd1adc647d6ffe060
Bob Donovan <Bob.Donovan@software-logistics.com>
2023-07-07 15:25:50 -0400

    Refactored LCDpic18.c to be more readable.   Also adjusted time delays to be much faster therefore making the writes to the LCD faster.   This change has allowed more time to process the GPS data.

--- LCDpic18.c
+++ LCDpic18.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 #include "LCDpic18.h"
 #define _XTAL_FREQ 8000000
-#define __delay_ms(x) _delay((unsigned long)((x)*(_XTAL_FREQ/4000.0)))
+//#define __delay_ms(x) _delay((unsigned long)((x)*(_XTAL_FREQ/4000.0)))
 // set up the timing for the LCD delays
-#define LCD_delay 5 // ~5mS
+#define LCD_delay 750 // ~5mS
 #define LCD_Startup 15 // ~15mS
 
 // Command set for Hitachi 44780U LCD display controller
@@ -58,120 +58,68 @@
 
 void LCD_Initialize()
 {
-// clear latches before enabling TRIS bits
-LCD_PORT = 0;
-
-TRISD = 0x00;
-
-// power up the LCD
-LCD_PWR = 1;
-
-// required by display controller to allow power to stabilize
-__delay_ms(LCD_Startup);
-
-// required by display initialization
-LCDPutCmd(0x32);
-
-// set interface size, # of lines and font
-LCDPutCmd(FUNCTION_SET);
-
-// turn on display and sets up cursor
-LCDPutCmd(DISPLAY_SETUP);
-
-DisplayClr();
-
-// set cursor movement direction
-LCDPutCmd(ENTRY_MODE);
-
+    LCD_PORT = 0;               // clear latches before enabling TRIS bits
+    TRISD = 0x00;
+    LCD_PWR = 1;                // power up the LCD
+    __delay_ms(LCD_Startup);    // required by display controller to allow power to stabilize
+    LCDPutCmd(0x32);            // required by display initialization
+    LCDPutCmd(FUNCTION_SET);    // set interface size, # of lines and font
+    LCDPutCmd(DISPLAY_SETUP);   // turn on display and sets up cursor
+    DisplayClr();
+    LCDPutCmd(ENTRY_MODE);      // set cursor movement direction
 }
 
 
 void LCDWriteNibble(char ch, char rs)
 {
-// always send the upper nibble
-ch = (ch >> 4);
 
-// mask off the nibble to be transmitted
-ch = (ch & 0x0F);
-
-// clear the lower half of LCD_PORT
-LCD_PORT = (LCD_PORT & 0xF0);
-
-// move the nibble onto LCD_PORT
-LCD_PORT = (LCD_PORT | ch);
-
-// set data/instr bit to 0 = insructions; 1 = data
-LCD_RS = rs;
-
-// RW - set write mode
-LCD_RW = 0;
-
-// set up enable before writing nibble
-LCD_EN = 1;
-
-// turn off enable after write of nibble
-LCD_EN = 0;
+    ch = (ch >> 4);             // always send the upper nibble
+    ch = (ch & 0x0F);           // mask off the nibble to be transmitted
+    LCD_PORT = (LCD_PORT & 0xF0);  // clear the lower half of LCD_PORT
+    LCD_PORT = (LCD_PORT | ch); // move the nibble onto LCD_PORT
+    LCD_RS = rs;                // set data/instr bit to 0 = insructions; 1 = data
+    LCD_RW = 0;                 // RW - set write mode
+    LCD_EN = 1;                 // set up enable before writing nibble
+    LCD_EN = 0;                 // turn off enable after write of nibble
 }
 
 void LCDPutChar(char ch)
 {
-__delay_ms(LCD_delay);
-
-//Send higher nibble first
-LCDWriteNibble(ch,data);
-
-//get the lower nibble
-ch = (ch << 4);
-
-// Now send the low nibble
-LCDWriteNibble(ch,data);
+    __delay_us(LCD_delay);
+    LCDWriteNibble(ch,data);    //Send higher nibble first
+    ch = (ch << 4);             //get the lower nibble
+    LCDWriteNibble(ch,data);    // Now send the low nibble
 }
 
 
 void LCDPutCmd(char ch)
 {
-__delay_ms(LCD_delay);
-
-//Send the higher nibble
-LCDWriteNibble(ch,instr);
-
-//get the lower nibble
-ch = (ch << 4);
-
-__delay_ms(1);
-
-//Now send the lower nibble
-LCDWriteNibble(ch,instr);
+    __delay_us(LCD_delay);
+    LCDWriteNibble(ch,instr);   //Send the higher nibble
+    ch = (ch << 4);             //get the lower nibble
+    __delay_us(LCD_delay);
+    LCDWriteNibble(ch,instr);  //Now send the lower nibble
 }
 
 
 void LCDPutStr(const char *str)
 {
-char i=0;
-
-// While string has not been fully traveresed
-while (str[i])
-{
-// Go display current char
-LCDPutChar(str[i++]);
-}
-
+    char i=0;
+    while (str[i])              // While string has not been fully traversed
+    {
+        LCDPutChar(str[i++]);   // Go display current char
+    }
 }
 
 void LCDGoto(char pos,char ln)
 {
-// if incorrect line or column
-if ((ln > (NB_LINES-1)) || (pos > (NB_COL-1)))
-{
-// Just do nothing
-return;
-}
 
-// LCD_Goto command
-LCDPutCmd((ln == 1) ? (0xC0 | pos) : (0x80 | pos));
-
-// Wait for the LCD to finish
-__delay_ms(LCD_delay);
+    if ((ln > (NB_LINES-1)) || (pos > (NB_COL-1)))  // if incorrect line or column
+    {
+        return;                 // Just do nothing
+    }
+    LCDPutCmd((ln == 1) ? (0xC0 | pos) : (0x80 | pos));  // LCD_Goto command
+    __delay_us(LCD_delay);      // Wait for the LCD to finish
 }
 /**
 End of File
